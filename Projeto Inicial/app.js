a7d0b28830ceb3bfae0b35c0fcfff3597b5de84e
Antonio Bigas <100382731+antoniobigas@users.noreply.github.com>
2025-02-11 08:56:16 -0300

    Update app.js

--- Projeto Inicial/app.js
+++ Projeto Inicial/app.js
@@ -1,18 +1,58 @@
 alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = parseInt(Math.random() * 10 + 1);
+console.log(numeroSecreto)
+let chute;
+let tentativas = 1;
 
-// Adicione um console.log para verificar o valor de "chute" após a entrada do usuário
-let chute = prompt('Escolha um número entre 1 e 10');
-console.log('Valor do chute:', chute);
-
-let numeroSecreto = 4;
-
-// Adicione um console.log para verificar a comparação entre "chute" e "numeroSecreto"
-console.log('Resultado da comparação:', chute == numeroSecreto);
-
-if (chute == numeroSecreto) {
-    alert('Acertou');
-} else {
-    // Adicione um console.log para verificar o valor de "numeroSecreto" quando o jogador erra
-    console.log('Valor do número secreto:', numeroSecreto);
-    alert('O número secreto era ' + numeroSecreto);
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+       break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`)
+        } else {
+            alert(`O número secreto é maior que ${chute}`)
+        }
+        // tentativas = tentativas + 1
+        tentativas++
+    }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+
+
+/* Desafio 2
+Desafios - Respostas
+Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+let contador = 1;
+while (contador <= 10) {
+    console.log(contador);
+    contador++;
+}
+Crie um contador que comece em 10 e vá até 0 usando um loop while. Mostre cada número.
+let contador = 10;
+while (contador >= 0) {
+    console.log(contador);
+    contador--;
+}
+Crie um programa de contagem regressiva. Peça um número ao usuario e conte deste número até 0, usando um loop while no console do navegador.
+let numeroMaximo = prompt("Digite um número para a contagem regressiva:");
+
+while (numeroMaximo >= 0) {
+    console.log(numeroMaximo);
+    numeroMaximo--;
+}
+Crie um programa de contagem progressiva. Peça um número ao usuario e conte de 0 até esté número, usando um loop while no console do navegador.
+let numeroMaximo = prompt("Digite um número para a contagem progregressiva:");;
+let contador = 0;
+
+while (contador <= numeroMaximo) {
+    console.log(contador);
+    contador++
+} */
+
